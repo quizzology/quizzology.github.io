bc6731c774901d69f8d450259c50c7b189b7a9de
Arnav K <30592878+arnalph@users.noreply.github.com>
2020-08-31 17:00:59 +0530

    Update game.js

--- game.js
+++ game.js
@@ -117,5 +117,5 @@ choices.forEach((choice) => {
 
 incrementScore = (num) => {
     score += num;
-    scoreText.innerText = score;
+    scoreText.innerHTML = score+'<small class="total">/50</small>';
 };
