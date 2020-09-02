6db112f90ad8d9bdb2e1405f5c42ebcd55647066
Arnav K <30592878+arnalph@users.noreply.github.com>
2020-09-02 16:56:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-/*window.onload = () => {
+window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('./sw.js');
   }
-}*/
+}
 if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
   
  document.getElementsByClassName('pwa-promote')[0].hidden='true';
