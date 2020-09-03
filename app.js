8a1ceafc43bd0e4174d4379774bd504837c47670
Arnav K <30592878+arnalph@users.noreply.github.com>
2020-09-03 20:13:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+document.getElementById('pwa-install').addEventListener('click', (e) => {
+  // Hide the app provided install promotion
+  hideMyInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+});
 if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
   
  document.getElementsByClassName('pwa-promote')[0].hidden='true';
