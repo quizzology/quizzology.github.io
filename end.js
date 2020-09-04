0c7de33f0c803c7574eebc28457e32a29227865f
Arnav K <30592878+arnalph@users.noreply.github.com>
2020-09-04 19:32:10 +0530

    Update end.js

--- end.js
+++ end.js
@@ -12,32 +12,21 @@ const MAX_HIGH_SCORES = 5;
 finalScore.innerHTML = mostRecentScore+finalScore.innerHTML;
 localStorage.setItem('lifeScore', Number(lifeScore)+mostRecentScore);
 
-document.getElementById('share-icon').addEventListener('click', function(){
-	
-						      navigator.share({
-                    title: 'I scored '+mostRecentScore+' in Quizzology!',
-                    text: 'Can you beat it?',
-                    url: 'https://quizzology.github.io'
-		    //files: 
-                })
-                .then(() => console.log('Successful share'))
-                .catch((error) => console.log('Error sharing', error)))
+const shareData = {
+  title: 'Quizzology',
+  text: 'I scored '+mostRecentScore+' in Quizzology can you?',
+  url: 'https://quizzology.github.io',
+  }
 
-saveHighScore = (e) => {
-    e.preventDefault();
+document.getElementById('share-icon').addEventListener('click', 
+  document.getElementsByClassName('pwa-promote')[0].addEventListener('click', async () => {
+  try {
+    await navigator.share(shareData)
+  } catch(err) {
+    console.log(err)
+  }
+});
 
-    const score = {
-        score: mostRecentScore,
-        name: username.value,
-    };
-    highScores.push(score);
-    highScores.sort((a, b) => b.score - a.score);
-    highScores.splice(5);
-
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-	localStorage.setItem('email', email.value)
-    window.location.assign('/');
-};
 if(Number(mostRecentScore)<=30)
 	console.log('You can do better!')
 if(Number(mostRecentScore)==40)
