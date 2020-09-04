267e7f443b2c43b18c731abbf628e1bb8e440c8c
Arnav K <30592878+arnalph@users.noreply.github.com>
2020-09-04 19:30:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,16 +25,5 @@ if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navig
   
  document.getElementsByClassName('pwa-promote')[0].hidden='true';
 }else{
-  const shareData = {
-  title: 'Quizzology',
-  text: 'Quiz yourself with random questions and test your skills. ',
-  url: 'https://quizzology.vercel.app',
-  }
-  document.getElementsByClassName('pwa-promote')[0].addEventListener('click', async () => {
-  try {
-    await navigator.share(shareData)
-  } catch(err) {
-    console.log(err)
-  }
-});
+  pass;
 }
