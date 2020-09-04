cc570c158fd443ccce0a56b61a2c4c415a93e652
Arnav K <30592878+arnalph@users.noreply.github.com>
2020-09-04 20:06:45 +0530

    Update end.js

--- end.js
+++ end.js
@@ -12,6 +12,22 @@ const MAX_HIGH_SCORES = 5;
 finalScore.innerHTML = mostRecentScore+finalScore.innerHTML;
 localStorage.setItem('lifeScore', Number(lifeScore)+mostRecentScore);
 
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+	localStorage.setItem('email', email.value)
+    window.location.assign('/');
+};
+
 var capture;
 document.onload = html2canvas(document.getElementsByTagName('html')[0]).then(canvas => {
     capture=(canvas)
