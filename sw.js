0e1140ef3798047df8d1232d5da017f99132a853
Arnav K <30592878+arnalph@users.noreply.github.com>
2020-09-05 20:02:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,3 +24,12 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+self.addEventListener('install',(e) => {});
