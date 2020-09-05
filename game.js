3c8541c997711b5d3344f13e34fcdd897ea53e3c
Arnav K <30592878+arnalph@users.noreply.github.com>
2020-09-05 15:46:28 +0530

    Update game.js

--- game.js
+++ game.js
@@ -117,5 +117,5 @@ choices.forEach((choice) => {
 
 incrementScore = (num) => {
     score += num;
-    scoreText.innerHTML = score;
+    scoreText.innerHTML = '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 53.867 53.867" style="enable-background:new 0 0 53.867 53.867; width:60px;margin-right:1rem;" xml:space="preserve"> <polygon style="fill:#EFCE4A;" points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798 	10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 "/></svg>'+score;
 };
